07b6293c93248962cdbd3fff6e80de0a5e258d78
Quentin Marque <quentinm2000@hotmail.fr>
2023-01-19 15:07:33 +0100

    Set up jeysey page

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,22 +1,24 @@
 import React from "react";
-import { VisibilityContext } from "react-horizontal-scrolling-menu";
 import Image from 'next/image'
+import Link from "next/link";
 
 export function Card({ title, itemId }: { title: string; itemId: string }) {
   return (
     <div
       role="button"
       tabIndex={0}
-      className="card border-2 border-black w-52 mx-2.5 rounded-lg"	
+      className="card border-2 border-black w-52 mx-2.5 rounded-lg"
     >
-      <div className=" h-44 flex justify-center items-center">
-      <Image
-      src="/images/maillots/maillot.png"
-      alt="Maillot"
-      width={100}
-      height={100}
-      />
-      </div>
+      <Link href={'/maillot'}>
+        <div className=" h-44 flex justify-center items-center">
+          <Image
+          src="/images/maillots/maillot.png"
+          alt="Maillot"
+          width={100}
+          height={100}
+          />
+        </div>
+      </Link>
       <hr className="w-1/2 m-auto mb-2"/>
       <div>{title}</div>
       <div className="rounded-lg p-px m-1 flex items-center justify-between">
